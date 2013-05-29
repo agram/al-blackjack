7f7ebc615064fedcf6752b71f81fe14d7e0ff81f
Albert Agram <albert.agram@gmail.com>
2013-05-29 09:24:20 +0000

    Ajaxified app

---
+++ public/application.js
@@ -0,0 +1,41 @@
+$(document).ready(function() {
+    player_hits();
+    player_stays();
+    dealer_hits();
+});
+
+function player_hits() {
+  $(document).on("click", "form#hit_form", function() {
+    $.ajax({
+      type: "POST",
+      url: "/game/player/hit"
+    }).done(function(resp) {
+      $("section#game").replaceWith(resp);
+    });
+    return false;
+  });
+}
+
+function player_stays () {
+  $(document).on("click", "form#stay_form", function() {
+    $.ajax({
+      type: "POST",
+      url: "/game/player/stay"
+    }).done(function(resp) {
+      $("section#game").replaceWith(resp);
+    });
+    return false;
+  });
+}
+
+function dealer_hits () {
+  $(document).on("click", "form#dealer_form", function() {
+    $.ajax({
+      type: "POST",
+      url: "/game/dealer/hit"
+    }).done(function(resp) {
+      $("section#game").replaceWith(resp);
+    });
+    return false;
+  });
+}
